775f0f35c828a4c0f75212861eea2f447516bdf9
Huseyin Akten <husakten@gmail.com>
2019-12-30 00:35:00 -0600

    typo edited

--- 11-privsubs.tf
+++ 11-privsubs.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "priv-2" {
 }
 resource "aws_subnet" "priv-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.pub-cidr[2]
+  cidr_block              = var.priv-cidr[2]
   map_public_ip_on_launch = false
   availability_zone = var.az[2]
 }
