5a74400bca5c0fc029f6148f311d39c09dec8ea0
Huseyin Akten <husakten@gmail.com>
2019-12-31 01:02:43 -0600

    sad

--- 22-pub-ec2.tf
+++ 22-pub-ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "bastion" {
   ami                    = var.ami-centos-7[var.region]
   subnet_id              = aws_subnet.pub-1.id
   key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.ssh.id]
+  vpc_security_group_ids = [aws_security_group.ssh.id,aws_security_group.web.id]
 
   tags = {
     Name = "Bastion"
