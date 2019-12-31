1f20e478a54b95cb0b33d90131bdddaede6c29e9
Huseyin Akten <husakten@gmail.com>
2019-12-31 13:58:36 -0600

    backend commented

--- 1-provider-backend.tf
+++ 1-provider-backend.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-terraform {
-    backend "s3" {
-    bucket = "terraform-tfstate-huseyin"
-    key    = "terraform-resources"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#     backend "s3" {
+#     bucket = "terraform-tfstate-huseyin"
+#     key    = "terraform-resources"
+#     region = "us-east-1"
+#   }
+# }
