0975ee58c8615db4a146d010ed5822fe590664c1
Huseyin Akten <husakten@gmail.com>
2020-01-03 13:47:32 -0600

    rtbs named

--- 08-igw-and-pub-rtb.tf
+++ 08-igw-and-pub-rtb.tf
@@ -7,21 +7,24 @@ resource "aws_route_table" "pub-rtb" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = aws_internet_gateway.igw.id
+    cidr_block  = "0.0.0.0/0"
+    gateway_id  = aws_internet_gateway.igw.id
+  }
+    tags = {
+    Name = "pub-rtb"
   }
 }
 
 
 resource "aws_route_table_association" "pub-1" {
-  route_table_id = aws_vpc.vpc.main_route_table_id
+  route_table_id = aws_vpc.vpc.pub-rtb.id
   subnet_id      = aws_subnet.pub-1.id
 }
 resource "aws_route_table_association" "pub-2" {
-  route_table_id = aws_vpc.vpc.main_route_table_id
+  route_table_id = aws_vpc.vpc.pub-rtb.id
   subnet_id      = aws_subnet.pub-2.id
 }
 resource "aws_route_table_association" "pub-3" {
-  route_table_id = aws_vpc.vpc.main_route_table_id
+  route_table_id = aws_vpc.vpc.pub-rtb.id
   subnet_id      = aws_subnet.pub-3.id
 }
--- 12-natgw-and-priv-rtb.tf
+++ 12-natgw-and-priv-rtb.tf
@@ -11,8 +11,11 @@ resource "aws_route_table" "priv-rtb" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat.id
+  }
+    tags = {
+    Name = "priv-rtb"
   }
 }
 
