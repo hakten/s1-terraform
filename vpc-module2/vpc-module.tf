4adee6c8ba26f7c3df4340864615d3cf4ed6a05e
Huseyin Akten <husakten@gmail.com>
2020-01-08 02:35:24 -0600

    second module

---
+++ vpc-module2/vpc-module.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "vpc" {
+  source = "./module"
+
+cidr            = "10.0.0.0/16"
+name            = "Wordpress"
+Environment     = "Test_New"
+
+azs             = ["us-east-1a","us-east-1b","us-east-1c"]
+public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+private_subnets = ["10.0.11.0/24","10.0.12.0/24","10.0.13.0/24"]
+
+}
