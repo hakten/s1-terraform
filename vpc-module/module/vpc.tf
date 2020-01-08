ac10c412f1357ab351ab5494c36191035797694f
Huseyin Akten <husakten@gmail.com>
2020-01-07 23:35:13 -0600

    public subnets added

--- vpc-module/module/vpc.tf
+++ vpc-module/module/vpc.tf
@@ -8,13 +8,13 @@ resource "aws_vpc" "vpc" {
   }
 }
 
-resource "aws_subnet" "public-subnet" {
+# resource "aws_subnet" "public-subnet" {
   
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.pub-cidr[0]
-  map_public_ip_on_launch = true
-  availability_zone = var.az[0]
-  tags = {
-    Name = "pub-1"
-  }
-}
+#   vpc_id                  = aws_vpc.vpc.id
+#   cidr_block              = var.pub-cidr[0]
+#   map_public_ip_on_launch = true
+#   availability_zone = var.az[0]
+#   tags = {
+#     Name = "pub-1"
+#   }
+# }
