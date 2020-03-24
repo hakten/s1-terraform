f2da1e35d4f612055068e250565ba099ab1fda30
Huseyin Akten <husakten@gmail.com>
2020-03-23 22:57:47 -0500

    vpc added

---
+++ vpc-variables/main.tf
@@ -0,0 +1,44 @@
+variable "cidr_block" {
+  default = "10.0.0.0"
+  description = "Cidr block of the vpc."
+}
+
+variable "vpc_name" {
+  default = "main_vpc"
+}
+
+variable "public_subnet" {
+  default = ["10.0.0.1","10.0.0.2","10.0.0.3"]
+}
+
+variable "az" {
+  default= ["eu-west-1a","eu-west-1b","eu-west-1c"]
+}
+
+
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.cidr_block}"
+  enable_dns_hostnames = "true"
+
+  tags = {
+    Name = "${var.vpc_name}"
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                     = "${aws_vpc.vpc.id}"
+  cidr_block                 = "${var.publi_subnet.*.cidr}"
+    availability_zone        = "${var.az.*}"
+  map_public_ip_on_launch    = true
+
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
